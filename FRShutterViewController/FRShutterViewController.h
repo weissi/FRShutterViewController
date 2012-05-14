6fbb1176a5fbdb268624b2065f1e3bbae5242f99
Johannes Weiß <github@tux4u.de>
2012-05-14 16:41:01 +0200

    Initial Commit

---
+++ FRShutterViewController/FRShutterViewController.h
@@ -0,0 +1,13 @@
+//
+//  FRShutterViewController.h
+//  FRShutterViewController
+//
+//  Created by Johannes Weiß on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRShutterViewController : NSObject
+
+@end
---
+++ FRShutterViewController/FRShutterViewController.m
@@ -0,0 +1,13 @@
+//
+//  FRShutterViewController.m
+//  FRShutterViewController
+//
+//  Created by Johannes Weiß on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FRShutterViewController.h"
+
+@implementation FRShutterViewController
+
+@end
